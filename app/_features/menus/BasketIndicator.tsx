55a2498852367d32d04d94f2705edd2a03ae2444
matinsekhavat <matinhyphen@gmail.com>
2024-07-30 19:55:54 +0330

    make price calculate as quantity + unitPrice and also make total order calculate as number of pizza quantities inside basket

--- app/_features/menus/BasketIndicator.tsx
+++ app/_features/menus/BasketIndicator.tsx
@@ -11,8 +11,18 @@ function BasketIndicator() {
       <div className="fixed bottom-0 left-0 right-0 h-14 bg-stone-800  text-stone-300 ">
         <div className="max-w-4xl mx-auto flex items-center justify-between h-full">
           <div className="flex items-center gap-10">
-            <p className=""> {basket.length} - pizzas🍕</p>
-            <p>{basket.reduce((acc, curr) => acc + curr.unitPrice, 0)} €</p>
+            <p className="">
+              {" "}
+              {basket.reduce((acc, curr) => acc + (curr.quantity ?? 0), 0)} -
+              pizzas🍕
+            </p>
+            <p>
+              {basket.reduce(
+                (acc, curr) => acc + curr.unitPrice * (curr.quantity ?? 0),
+                0
+              )}{" "}
+              €
+            </p>
           </div>
           <Link href="/cart">open Cart &rarr;</Link>
         </div>
