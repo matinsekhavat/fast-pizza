f355b922a4dcfc6f808927aa5faf556ff2913a1b
matinsekhavat <matinhyphen@gmail.com>
2024-07-30 19:33:57 +0330

    make some margin from bottom to fix conflict MenuList with Basket Indicator

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -5,7 +5,7 @@ async function page() {
   const menus = await res.json();
   if (!menus.data.length) return <p>fetching</p>;
   return (
-    <div className="my-2 max-w-3xl mx-auto">
+    <div className="my-2 max-w-3xl mx-auto mb-20">
       <MenuList menus={menus.data} />
     </div>
   );
