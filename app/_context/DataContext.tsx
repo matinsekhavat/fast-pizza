73cf21b983865c2cc8a110d0dad09af4a29aaba4
matinsekhavat <matinhyphen@gmail.com>
2024-07-30 19:35:53 +0330

    add basket list and  add to Basket handler

--- app/_context/DataContext.tsx
+++ app/_context/DataContext.tsx
@@ -1,18 +1,25 @@
 "use client";
 import { ReactNode, createContext, useContext, useState } from "react";
+import { MenuItemProps } from "../_features/menus/MenuList";
 
 type DataProps = {
   name: string;
   setName: (name: string) => void;
+  basket: MenuItemProps[];
+  addToCartHandler: (data: MenuItemProps) => void;
 };
 
 const DataContext = createContext<DataProps | null>(null);
 
 const DataProvider = ({ children }: { children: ReactNode }) => {
   const [name, setName] = useState<string>("");
+  const [basket, setBasket] = useState<MenuItemProps[]>([]);
+  function addToCartHandler(data: MenuItemProps): void {
+    setBasket((prev) => [...prev, data]);
+  }
 
   return (
-    <DataContext.Provider value={{ name, setName }}>
+    <DataContext.Provider value={{ name, setName, basket, addToCartHandler }}>
       {children}
     </DataContext.Provider>
   );
