617fde4fb07519777256ef63989706c0a90991ea
matinsekhavat <matinhyphen@gmail.com>
2024-07-30 20:07:40 +0330

    implement  logic of decreaseQuantityHandler into addCartButton component

--- app/_ui/AddCardButton.tsx
+++ app/_ui/AddCardButton.tsx
@@ -4,12 +4,20 @@ import { useData } from "../_context/DataContext";
 import { MenuItemProps } from "../_features/menus/MenuList";
 
 function AddCardButton({ menu }: { menu: MenuItemProps }) {
-  const { addToCartHandler, basket, increaseBasketHandler } = useData();
+  const {
+    addToCartHandler,
+    basket,
+    increaseBasketHandler,
+    decreaseBasketHandler,
+  } = useData();
   const isInBasket = basket.find((item) => item.id === menu.id);
 
   return isInBasket ? (
     <div className="flex items-center gap-2">
-      <button className="flex items-center justify-center p-2 size-8 bg-yellow-500 rounded-full">
+      <button
+        className="flex items-center justify-center p-2 size-8 bg-yellow-500 rounded-full"
+        onClick={() => decreaseBasketHandler(menu.id)}
+      >
         <span>-</span>
       </button>
       <p>{isInBasket.quantity}</p>
