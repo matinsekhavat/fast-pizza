3f51f0d73e7dd6c1f7d56211589d2db5e6589539
matinsekhavat <matinhyphen@gmail.com>
2024-08-01 18:16:38 +0330

    implement GoBackButton into Cart page.tsx file

---
+++ app/cart/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GoBackButton from "../_ui/GoBackButton";
+
+function page() {
+  return (
+    <div className="max-w-3xl mx-auto my-4">
+      <GoBackButton> ←&nbsp; Back to menu</GoBackButton>
+    </div>
+  );
+}
+
+export default page;
